d0007380d635d99da72cfd0424dcf3d3bdb42fa2
ShreyashPatodia <patodiashreyash32@gmail.com>
2017-05-20 01:24:54 +1000

    Adding files

--- language-detection.py
+++ language-detection.py
@@ -1,13 +1,16 @@
 import json
+from string import digits, punctuation
 from html.parser import HTMLParser
 from collections import defaultdict
 from pandas import DataFrame
-from string import digits
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
+import re
 
 def load_json(content):
+
     json_data = []
 
     for line in content:
@@ -16,6 +19,7 @@ def load_json(content):
     return json_data
 
 def train_simple_classifier(training_file):
+    
     with open(training_file) as file:
         content = file.readlines()
     json_data = load_json(content)
@@ -23,16 +27,34 @@ def train_simple_classifier(training_file):
     # create_ngrams(json_data, n)
     data_frame = build_dataframe(clean_json_data)
     pipeline = Pipeline([
-        ('vectorizer', CountVectorizer(analyzer='char', ngram_range=(1, 4))),
+        ('vectorizer', CountVectorizer(analyzer='char', ngram_range=(2, 4))),
         ('classifier', MultinomialNB())
     ])
     pipeline.fit(data_frame['text'].values, data_frame['class'].values)
     return pipeline
 
+def train_svm_classifier(training_file):
+    with open(training_file) as file:
+        content = file.readlines()
+    print("Going to get data")
+    json_data = load_json(content)
+    print("Data got")
+    clean_json_data = clean_data(json_data)
+    # create_ngrams(json_data, n)
+    data_frame = build_dataframe(clean_json_data)
+    pipeline = Pipeline([
+        ('vectorizer', CountVectorizer(analyzer='char', ngram_range=(2, 4))),
+        ('classifier', LinearSVC())
+    ])
+    pipeline.fit(data_frame['text'].values, data_frame['class'].values)
+    return pipeline
+
+
 def clean_data(json_data):
     clean_json_data = json_data
     html_parser = HTMLParser()
     remove_digits = str.maketrans('', '', digits)
+    # remove_punctutation = re.compile(punctuation())
     count = 0
     for obj in clean_json_data:
         obj['text'] = html_parser.unescape(obj['text'])
@@ -68,7 +90,53 @@ def create_ngrams(json_data, n):
     for lang in language_dict:
         input_languages.append(lang)
 
-def main():
+def svm_system():
+    pipeline = train_svm_classifier("train.json")
+    print("Training done!")
+    with open("dev.json") as file:
+        dev_content = file.readlines()
+    json_dev_data = load_json(dev_content)
+    to_predict = []
+    answers = []
+    for obj in json_dev_data:
+        to_predict.append(obj['text'])
+        answers.append(obj['lang'])
+    print("Fine here")
+    # predicted_probabilities = pipeline.predict_proba(to_predict)
+    predictions = pipeline.predict(to_predict)
+    score = pipeline.decision_function(to_predict)
+    '''for i in range(len(score)):
+        if(answers[i] == 'unk'):
+            print(score[i])'''
+    final_predictions = []
+    for i in range(len(predictions)):
+        if max(score[i]) < 0.5:
+            print(max(score[i]))
+            print(str(predictions[i]) + " " + str(answers[i]))
+            final_predictions.append('unk')
+        else:
+            final_predictions.append(predictions[i])
+
+    count = 0
+    total = 0
+    unks_wrong = 0
+    for i in range(len(answers)):
+        if final_predictions[i] == answers[i]:
+            count += 1
+        else:
+            # print("Predicted:" + str(final_predictions[i]) + " Real:" + str(answers[i]))
+            # print(str(predicted_probabilities[i]))
+            if answers[i] == 'unk':
+                unks_wrong += 1
+
+        total += 1
+    '''for i in range(len(answers)):
+        if answers[i] == 'unk':
+            print(str(max(predicted_probabilities[i])))
+    '''
+    print(str(count) + " " + str(total) + " "  + str(unks_wrong))
+
+def naive_bayes_system():
     pipeline = train_simple_classifier("train.json")
     with open("dev.json") as file:
         dev_content = file.readlines()
@@ -82,7 +150,7 @@ def main():
     predictions = pipeline.predict(to_predict)
     final_predictions = []
     for i in range(len(predictions)):
-        if max(predicted_probabilities[i]) < 0.60:
+        if max(predicted_probabilities[i]) < 0.30:
             print(max(predicted_probabilities[i]))
             print(str(predictions[i]) + " " + str(answers[i]))
             final_predictions.append('unk')
@@ -96,17 +164,17 @@ def main():
         if predictions[i] == answers[i]:
             count += 1
         else:
-            print("Predicted:" + str(predictions[i]) + " Real:" + str(answers[i]))
-            print(str(predicted_probabilities[i]))
+            # print("Predicted:" + str(final_predictions[i]) + " Real:" + str(answers[i]))
+            # print(str(predicted_probabilities[i]))
             if answers[i] == 'unk':
                 unks_wrong += 1
+
         total += 1
-    '''for i in range(len(answers)):
-        if answers[i] == 'unk':
-            print(str(max(predicted_probabilities[i])))
-    '''
     print(str(count) + " " + str(total) + " "  + str(unks_wrong))
     
+def main():
+    svm_system()
+
 main()
 
 
